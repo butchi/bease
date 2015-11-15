f5d3c403ae0ee50b40b54d273cb7d522d28a6fae
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-11-16 07:11:01 +0900

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 
-gulp.task("default", function () {
+gulp.task('watch', function(){
+    gulp.watch('src/bease.js', ['babel']);
+});
+
+gulp.task("babel", function () {
   return gulp.src("src/bease.js")
     .pipe(babel())
     .pipe(gulp.dest("dist"));
 });
+
+gulp.task("default", ['babel']);
