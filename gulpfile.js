21a85e0b0bed770d36bc05e4cf75ca63f7586390
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-11-18 09:09:20 +0900

    add watch, plumber, notify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
 
-gulp.task("default", function () {
-  return gulp.src("src/bease.js")
+gulp.task('babel', function () {
+  return gulp.src('src/bease.js')
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>") //<-
+    }))
     .pipe(babel())
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task("watch", function () {
+  gulp.watch('src/bease.js', ['babel']);
+});
+
+gulp.task('default', ['babel']);
