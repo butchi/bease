68b315f2c75f96e7f8cab72f0a875317ce2299fe
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-11-20 15:30:09 +0900

    fix draw method

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,40 +81,14 @@ var CubicBezier = (function () {
       var curve = new Curve(cbPts);
       // console.log(curve.stringify(2));
 
-      if (this.path) {
-        this.path.remove();
-      }
-      this.path = this.paper.path(curve.stringify(5));
+      this.path && this.path.remove();
+      this.path = this.paper.path(curve.stringify(5)).attr('stroke-width', 2);
       this.path.scale(200, -200, 0, 1);
     }
   }]);
 
   return CubicBezier;
 })();
-
-function moveDrag(dx, dy) {
-  dx /= 200;
-  dy /= -200;
-  this.attr("cx", this._cx + dx).attr("cy", this._cy + dy);
-
-  var c1 = circleArr[1];
-  var c2 = circleArr[2];
-  var a = c1.attr('cx');
-  var b = c1.attr('cy');
-  var c = c2.attr('cx');
-  var d = c2.attr('cy');
-  bezier.render(a, b, c, d);
-}
-
-function startDrag() {
-  this._cx = this.attr("cx");
-  this._cy = this.attr("cy");
-}
-
-function stopDrag() {
-  delete this._cx;
-  delete this._cy;
-}
 
 var bezier;
 var defaultParam = {
@@ -126,6 +100,13 @@ var defaultParam = {
 
 var ptArr = [new Point(0, 0), new Point(defaultParam.a, defaultParam.b), new Point(defaultParam.c, defaultParam.d), new Point(1, 1)];
 var circleArr = [];
+
+var line1;
+var line2;
+
+Raphael.fn.line = function (pt1, pt2) {
+  return this.path(Raphael.format("M{0},{1} {2},{3}", pt1.x, pt1.y, pt2.x, pt2.y)).attr('stroke-width', 1).scale(200, -200, 0, 1);
+};
 
 Raphael(function () {
   var $canvas = $('.canvas');
@@ -140,11 +121,42 @@ Raphael(function () {
     circleArr.push(ptElm);
   });
 
+  line1 = paper.line(new Point(0, 0), new Point(defaultParam.a, defaultParam.b));
+  line2 = paper.line(new Point(defaultParam.c, defaultParam.d), new Point(1, 1));
   circleArr[1].drag(moveDrag, startDrag, stopDrag);
   circleArr[2].drag(moveDrag, startDrag, stopDrag);
 
   // paper.path("M10,10L20,30L30,100");
 });
+
+function moveDrag(dx, dy) {
+  dx /= 200;
+  dy /= -200;
+  this.attr("cx", this._cx + dx).attr("cy", this._cy + dy);
+
+  var c1 = circleArr[1];
+  var c2 = circleArr[2];
+  var a = c1.attr('cx');
+  var b = c1.attr('cy');
+  var c = c2.attr('cx');
+  var d = c2.attr('cy');
+  bezier.render(a, b, c, d);
+
+  line1.remove();
+  line2.remove();
+  line1 = this.paper.line(new Point(0, 0), new Point(a, b));
+  line2 = this.paper.line(new Point(c, d), new Point(1, 1));
+}
+
+function startDrag() {
+  this._cx = this.attr("cx");
+  this._cy = this.attr("cy");
+}
+
+function stopDrag() {
+  delete this._cx;
+  delete this._cy;
+}
 
 var bease = new Bease({
   func: 'linear',
