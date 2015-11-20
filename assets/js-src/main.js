ee3afe5434b3dc4379ffc7ab1910dfa42a8ee2d2
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2015-11-20 14:49:58 +0900

    implement curve manipulator

--- assets/js-src/main.js
+++ assets/js-src/main.js
@@ -1,3 +1,6 @@
+var bezier;
+var circleArr = [];
+
 Raphael(function(){
   var $canvas = $('.canvas');
   var paper = Raphael($canvas.get(0), 200, 200);
@@ -9,40 +12,37 @@ Raphael(function(){
     new Point(1,   1  )
   ];
 
-  var cb = cubicBezier(0.2, 0.5, 0.8, 0.2);
+  bezier = new CubicBezier(paper);
+  bezier.render(0.2, 0.5, 0.8, 0.2);
 
-  var cbPts = [];
-  var i;
-  var len = 101;
-  for(i = 0; i < len; i++) {
-    cbPts.push(cb(i / len));
-  }
-  var curve = new Curve(cbPts);
-  // console.log(curve.stringify(2));
-  var path = paper.path(curve.stringify(5));
-  path.scale(200, -200, 0, 1);
-
-  var circleArr = [];
   ptArr.forEach((elm) => {
-    var ptElm = paper.circle(elm.x, elm.y, 0.01)
+    var ptElm = paper.circle(elm.x, elm.y, 0.02)
       .attr({fill:"red",stroke:"none"});
     ptElm.scale(200, -200, 0, 1)
     circleArr.push(ptElm);
   })
 
-  circleArr[1].drag(startDrag, moveDrag, stopDrag);
-  circleArr[2].drag(startDrag, moveDrag, stopDrag);
+  circleArr[1].drag(moveDrag, startDrag, stopDrag);
+  circleArr[2].drag(moveDrag, startDrag, stopDrag);
 
   // paper.path("M10,10L20,30L30,100");
 });
 
-function startDrag(dx,dy){
+function moveDrag(dx,dy){
   dx /=  200;
   dy /= -200;
   this.attr("cx", this._cx + dx).attr("cy", this._cy + dy);
+
+  var c1 = circleArr[1];
+  var c2 = circleArr[2];
+  var a = c1.attr('cx');
+  var b = c1.attr('cy');
+  var c = c2.attr('cx');
+  var d = c2.attr('cy');
+  bezier.render(a, b, c, d);
 }
 
-function moveDrag(){
+function startDrag(){
   this._cx = this.attr("cx");
   this._cy = this.attr("cy");        
 }
@@ -86,20 +86,38 @@ class Curve {
   }
 }
 
-var curve = new Curve(
-  new Point(0, 0),
-  new Point(0.2, 0.6),
-  new Point(0.8, 0.3),
-  new Point(1, 1)
-);
+class CubicBezier {
+  constructor(paper) {
+    this.paper = paper;
+  }
 
-function cubicBezier(a, b, c, d) {
-  return function(t) {
-    return new Point(
-      t*t*t + 3*t*t*(1-t)*c + 3*t*(1-t)*(1-t)*a,
-      t*t*t + 3*t*t*(1-t)*d + 3*t*(1-t)*(1-t)*b
-    );
-  };
+  cubicBezierFunction(a, b, c, d) {
+    return function(t) {
+      return new Point(
+        t*t*t + 3*t*t*(1-t)*c + 3*t*(1-t)*(1-t)*a,
+        t*t*t + 3*t*t*(1-t)*d + 3*t*(1-t)*(1-t)*b
+      );
+    };
+  }
+
+  render(a, b, c, d) {
+    var cb = this.cubicBezierFunction(a, b, c, d);
+
+    var cbPts = [];
+    var i;
+    var len = 101;
+    for(i = 0; i < len; i++) {
+      cbPts.push(cb(i / len));
+    }
+    var curve = new Curve(cbPts);
+    // console.log(curve.stringify(2));
+
+    if(this.path) {
+      this.path.remove();
+    }
+    this.path = this.paper.path(curve.stringify(5));
+    this.path.scale(200, -200, 0, 1);
+  }
 }
 
 var bease = new Bease({
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,45 +4,45 @@ var _createClass = (function () { function defineProperties(target, props) { for
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+var bezier;
+var circleArr = [];
+
 Raphael(function () {
   var $canvas = $('.canvas');
   var paper = Raphael($canvas.get(0), 200, 200);
 
   var ptArr = [new Point(0, 0), new Point(0.2, 0.5), new Point(0.8, 0.2), new Point(1, 1)];
 
-  var cb = cubicBezier(0.2, 0.5, 0.8, 0.2);
+  bezier = new CubicBezier(paper);
+  bezier.render(0.2, 0.5, 0.8, 0.2);
 
-  var cbPts = [];
-  var i;
-  var len = 101;
-  for (i = 0; i < len; i++) {
-    cbPts.push(cb(i / len));
-  }
-  var curve = new Curve(cbPts);
-  // console.log(curve.stringify(2));
-  var path = paper.path(curve.stringify(5));
-  path.scale(200, -200, 0, 1);
-
-  var circleArr = [];
   ptArr.forEach(function (elm) {
-    var ptElm = paper.circle(elm.x, elm.y, 0.01).attr({ fill: "red", stroke: "none" });
+    var ptElm = paper.circle(elm.x, elm.y, 0.02).attr({ fill: "red", stroke: "none" });
     ptElm.scale(200, -200, 0, 1);
     circleArr.push(ptElm);
   });
 
-  circleArr[1].drag(startDrag, moveDrag, stopDrag);
-  circleArr[2].drag(startDrag, moveDrag, stopDrag);
+  circleArr[1].drag(moveDrag, startDrag, stopDrag);
+  circleArr[2].drag(moveDrag, startDrag, stopDrag);
 
   // paper.path("M10,10L20,30L30,100");
 });
 
-function startDrag(dx, dy) {
+function moveDrag(dx, dy) {
   dx /= 200;
   dy /= -200;
   this.attr("cx", this._cx + dx).attr("cy", this._cy + dy);
+
+  var c1 = circleArr[1];
+  var c2 = circleArr[2];
+  var a = c1.attr('cx');
+  var b = c1.attr('cy');
+  var c = c2.attr('cx');
+  var d = c2.attr('cy');
+  bezier.render(a, b, c, d);
 }
 
-function moveDrag() {
+function startDrag() {
   this._cx = this.attr("cx");
   this._cy = this.attr("cy");
 }
@@ -101,13 +101,44 @@ var Curve = (function () {
   return Curve;
 })();
 
-var curve = new Curve(new Point(0, 0), new Point(0.2, 0.6), new Point(0.8, 0.3), new Point(1, 1));
+var CubicBezier = (function () {
+  function CubicBezier(paper) {
+    _classCallCheck(this, CubicBezier);
 
-function cubicBezier(a, b, c, d) {
-  return function (t) {
-    return new Point(t * t * t + 3 * t * t * (1 - t) * c + 3 * t * (1 - t) * (1 - t) * a, t * t * t + 3 * t * t * (1 - t) * d + 3 * t * (1 - t) * (1 - t) * b);
-  };
-}
+    this.paper = paper;
+  }
+
+  _createClass(CubicBezier, [{
+    key: "cubicBezierFunction",
+    value: function cubicBezierFunction(a, b, c, d) {
+      return function (t) {
+        return new Point(t * t * t + 3 * t * t * (1 - t) * c + 3 * t * (1 - t) * (1 - t) * a, t * t * t + 3 * t * t * (1 - t) * d + 3 * t * (1 - t) * (1 - t) * b);
+      };
+    }
+  }, {
+    key: "render",
+    value: function render(a, b, c, d) {
+      var cb = this.cubicBezierFunction(a, b, c, d);
+
+      var cbPts = [];
+      var i;
+      var len = 101;
+      for (i = 0; i < len; i++) {
+        cbPts.push(cb(i / len));
+      }
+      var curve = new Curve(cbPts);
+      // console.log(curve.stringify(2));
+
+      if (this.path) {
+        this.path.remove();
+      }
+      this.path = this.paper.path(curve.stringify(5));
+      this.path.scale(200, -200, 0, 1);
+    }
+  }]);
+
+  return CubicBezier;
+})();
 
 var bease = new Bease({
   func: 'linear',
